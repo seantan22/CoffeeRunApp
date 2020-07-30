0ee6c2693e0a1ea82be45875e0f10168390eeb03
alexander <alexander@MacBook-Pro-5.local>
2020-07-29 23:30:02 -0400

    Added frontend basic setup

---
+++ CoffeeRun/CoffeeRun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoffeeRun
+//
+//  Created by alexander on 07-29-2020.
+//  Copyright © 2020 CoffeeRun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
