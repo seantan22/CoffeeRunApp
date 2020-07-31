071a77c2eb66b01d54d7e0b0d56009d08d726a76
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-31 17:30:17 -0400

    Initialize Xcode Project
    - Tabbed App
    - Swift
    - Storyboard

---
+++ CoffeeRun/CoffeeRun/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CoffeeRun
+//
+//  Created by Sean Tan on 7/31/20.
+//  Copyright © 2020 CoffeeRun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
