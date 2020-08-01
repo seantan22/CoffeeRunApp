fd32d3b8adfbe0a41bf694b5682296c69614ec70
Sean Tan <sean.tan@mail.mcgill.ca>
2020-08-01 12:35:25 -0400

    Update login page
    - User can enter email, password then click "Sign In"
    - HTTP request is sent and prints the correct response in console
    - Text fields return method

--- CoffeeRun/CoffeeRun/LoginViewController.swift
+++ CoffeeRun/CoffeeRun/LoginViewController.swift
@@ -8,23 +8,72 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField  == emailTextField {
+            textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            textField.resignFirstResponder()
+        }
+        return true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Actions
+    @IBAction func loginUser(_ sender: UIButton) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        login(email: email, password: password)
+   }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        emailTextField.delegate = self;
+        passwordTextField.delegate = self;
     }
-    */
+    
+    
+    
+    func login(email: String, password: String) {
+        
+        let session = URLSession.shared
+        
+        guard let url = URL(string: "http:/localhost:5000/login") else {
+            print("Error: Cannot create URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonLogin = [
+            "email": email,
+            "password": password
+        ]
+        
+        let dataLogin: Data
+        do {
+            dataLogin = try JSONSerialization.data(withJSONObject: jsonLogin, options: [] )
+        } catch {
+            print("Error: Unable to convert JSON to Data object")
+            return
+        }
+       
+        let task = session.uploadTask(with: request, from: dataLogin) { data, response, error in
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print(dataString)
+            }
+        }
+        
+        task.resume()
+    }
 
 }
