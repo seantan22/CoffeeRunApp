19b4c50b11cae870739aab8b393d7de13904d284
Sean Tan <sean.tan@mail.mcgill.ca>
2020-07-31 20:08:24 -0400

    Added login page UI and DesignableView

---
+++ CoffeeRun/CoffeeRun/DesignableView.swift
@@ -0,0 +1,37 @@
+//
+//  DesignableView.swift
+//  CoffeeRun
+//
+//  Created by Sean Tan on 7/31/20.
+//  Copyright © 2020 CoffeeRun. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+    
+}
