3fc7514624b946ce9736eb2f354d00fb1608078a
Sean Tan <sean.tan@mail.mcgill.ca>
2020-08-02 15:48:33 -0400

    Update ProfileViewController.swift

--- CoffeeRun/CoffeeRun/ProfileViewController.swift
+++ CoffeeRun/CoffeeRun/ProfileViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    //MARK: Actions
+    @IBAction func logoutUser(_ sender: UIButton) {
+        
+        logout(user_id: UserDefaults.standard.string(forKey: "user_id")!)
+        
+        // Transition to Login Screen
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let loginNavigationController = storyboard.instantiateViewController(withIdentifier: "LoginNavigationController")
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?
+            .changeRootViewController(loginNavigationController)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    func logout(user_id: String) {
+        
+        UserDefaults.standard.removeObject(forKey: "user_id")
+        
     }
     
 }
