b868bc30b9827b99412ef06de078dc567ea955df
Sean Tan <sean.tan@mail.mcgill.ca>
2020-08-13 18:18:32 -0400

    Added History pages

---
+++ CoffeeRun/CoffeeRun/OrderHistoryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OrderHistoryViewController.swift
+//  CoffeeRun
+//
+//  Created by Sean Tan on 8/13/20.
+//  Copyright © 2020 CoffeeRun. All rights reserved.
+//
+
+import UIKit
+
+class OrderHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
