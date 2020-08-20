07b798e7e79fb32f670313ec3bcd5f2d51afa29f
Sean Tan <sean.tan@mail.mcgill.ca>
2020-08-20 09:33:20 -0400

    Updated library pictures

--- CoffeeRun/CoffeeRun/SelectLibraryViewController.swift
+++ CoffeeRun/CoffeeRun/SelectLibraryViewController.swift
@@ -51,7 +51,6 @@ class SelectLibraryViewController: UIViewController {
         redpathButton.libraryImage()
         lawButton.libraryImage()
         musicButton.libraryImage()
-        
     }
     
 }
